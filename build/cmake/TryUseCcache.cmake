347076dcb0ed57ccf7842b5d40e774317d17d18c
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-03-16 23:01:41 -0400

    attempt increase ccache hit rate

--- build/cmake/TryUseCcache.cmake
+++ build/cmake/TryUseCcache.cmake
@@ -15,19 +15,19 @@ if(CCACHE_PROGRAM)
 
         # not worked
 
-    elseif(CC_IS_CLANG)
+    elseif(CC_IS_CLANG OR CC_IS_GCC)
+
+        # Set up wrapper scripts
+        set(C_LAUNCHER   "${CCACHE_PROGRAM}")
+        set(CXX_LAUNCHER "${CCACHE_PROGRAM}")
+        configure_file(${PROJECT_SOURCE_DIR}/build/launch-c.in   launch-c)
+        configure_file(${PROJECT_SOURCE_DIR}/build/launch-cxx.in launch-cxx)
+        execute_process(COMMAND chmod a+rx
+                        "${CMAKE_BINARY_DIR}/launch-c"
+                        "${CMAKE_BINARY_DIR}/launch-cxx"
+        )      
 
        if (CMAKE_GENERATOR STREQUAL "Xcode")
-
-           # Set up wrapper scripts
-           set(C_LAUNCHER   "${CCACHE_PROGRAM}")
-           set(CXX_LAUNCHER "${CCACHE_PROGRAM}")
-           configure_file(${PROJECT_SOURCE_DIR}/build/launch-c.in   launch-c)
-           configure_file(${PROJECT_SOURCE_DIR}/build/launch-cxx.in launch-cxx)
-           execute_process(COMMAND chmod a+rx
-                            "${CMAKE_BINARY_DIR}/launch-c"
-                            "${CMAKE_BINARY_DIR}/launch-cxx"
-           )
 
            # Set Xcode project attributes to route compilation and linking
            # through our scripts
@@ -37,19 +37,11 @@ if(CCACHE_PROGRAM)
            set(CMAKE_XCODE_ATTRIBUTE_LDPLUSPLUS "${CMAKE_BINARY_DIR}/launch-cxx")
            message(STATUS "Using ccache")
        else()
-           set(ENV{CCACHE_CPP2} true)
-           set(CMAKE_C_COMPILER_LAUNCHER   "${CCACHE_PROGRAM}")
-           set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+           # Support Unix Makefiles and Ninja
+           set(CMAKE_C_COMPILER_LAUNCHER   "${CMAKE_BINARY_DIR}/launch-c")
+           set(CMAKE_CXX_COMPILER_LAUNCHER "${CMAKE_BINARY_DIR}/launch-cxx")
            message(STATUS "Using ccache")
        endif()
-
-    elseif(CC_IS_GCC)
-
-        # Support Unix Makefiles and Ninja
-        set(ENV{CCACHE_CPP2} true)
-        set(CMAKE_C_COMPILER_LAUNCHER   "${CCACHE_PROGRAM}")
-        set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
-        message(STATUS "Using ccache")
 
     endif()
 
